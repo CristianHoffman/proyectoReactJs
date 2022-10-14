aa18dd224f77b3c7ff40df28471af2682c3b6d6a
Cristian Hoffman <cristian.hoffman99@gmail.com>
2022-10-13 21:40:44 -0300

    datos en firebase

--- src/components/CartView/CartView.js
+++ src/components/CartView/CartView.js
@@ -1,10 +1,44 @@
 import { useCartContext } from "../Context/CartContext"
 import { Link } from "react-router-dom"
 import CartItem from "../CartItem/CartItem"
+import { db } from "../firebase/firebase"
+import {collection } from "firebase/firestore"
+import { addDoc } from "firebase/firestore"
+import "../CartView/CartView.css"
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 
 export const CartView = () =>{
  const {carrito, precioTotal} = useCartContext()
-     
+ 
+
+ const order = {
+    buyer: {
+        name: "Cristian",
+        email: "asd@gmail.com",
+        telefono: "114142",
+        direccion: "as123"
+    },
+    item: carrito.map(product => ({ id: product.id, title: product.product, price: product.price, quantity: product.quantity})),
+    total: precioTotal(),
+ }
+    
+ const handlerClick = () => {
+    const orderCollection = collection(db, "ventas");
+    addDoc(orderCollection, order)
+    .then(({id}) =>console.log(id))
+    toast('Tu compra ha sido realizada con exito!', {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+        });
+ }
     if(carrito.length === 0){
         return (
             <>
@@ -19,7 +53,12 @@ export const CartView = () =>{
         {
             carrito.map(product => <CartItem key={product.id} product={product}/>)
         }
+        <div className="contenedor-comprar">
         <p className="text-center fs-2 text-info">Total: {precioTotal()}</p>
+
+        <button onClick={handlerClick} className="btn btn-success">Comprar</button>
+        </div>
+        <ToastContainer/>
         </>
     )
 }
--- src/components/Conteiner/ItemListConteiner.js
+++ src/components/Conteiner/ItemListConteiner.js
@@ -1,8 +1,6 @@
 import React, {useState, useEffect} from "react";
 import ItemList from "../ItemList/ItemList";
-import { Productos } from "../../assets/Productos";
 import { useParams } from "react-router-dom";
-import { BounceLoader } from "react-spinners";
 import { db } from "../firebase/firebase";
 import { collection, getDocs, query, where} from "firebase/firestore";
 
@@ -10,7 +8,7 @@ import { collection, getDocs, query, where} from "firebase/firestore";
 const ItemListContainer = ({greeting}) =>{
     let { categoriaId } = useParams();
     const [listProducs, setListProducts] = useState([])
-    const [cargando, setCargando] = useState({})
+    
     
     useEffect(() =>{
         const queryCollection = collection(db, 'products')
@@ -22,7 +20,7 @@ const ItemListContainer = ({greeting}) =>{
         .then(res => setListProducts(res.docs.map(product => ({id: product.id, ...product.data() }))))
     
        
-        setCargando(false)
+        
     } else {
         getDocs(queryCollection)
         .then(res => setListProducts(res.docs.map(product => ({id: product.id, ...product.data() }))))
@@ -37,12 +35,8 @@ const ItemListContainer = ({greeting}) =>{
     return (
         <>
         <h2 className="text-center pt-4 fs-2">{greeting}</h2>
-        {
-            cargando
-            ? <BounceLoader color="#7c36d6" />
-            : <ItemList listProducts={listProducs}/>
-        }
-    </>
+        <ItemList listProducts={listProducs}/>
+       </>
     )
 }
 
